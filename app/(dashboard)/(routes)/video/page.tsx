0dd782aeac4c2ca539e33967c1e93fb60705f35b
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-06-09 11:43:38 +0530

    added video gen

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,189 @@
+"use client";
+
+import * as z from "zod";
+import { Download, ImageIcon, MessageSquare, Video } from "lucide-react";
+import { Heading } from "../../../../components/Heading";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../../../../components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../../../../components/ui/form";
+import { Button } from "../../../../components/ui/button";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Empty } from "../../../../components/Empty";
+import { Loader } from "../../../../components/Loader";
+import { BoatAvatar } from "../../../../components/BoatAvatar";
+import { UserAvatar } from "../../../../components/UserAvatar";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../../../components/ui/select";
+import { SelectContent } from "@radix-ui/react-select";
+import Image from "next/image";
+
+const VideoGeneration = () => {
+  const router = useRouter();
+  const [videos, setVideo] = useState<string>();
+  const formSchema = z.object({
+    prompt: z.string().min(1, {
+      message: "Video Prompt is required.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+      const response = await axios.post("/api/video", values);
+
+      console.log(response, "cidep");
+
+      setVideo(response.data[0]);
+
+      form.reset();
+    } catch (error: any) {
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Video Generation"
+        description="Turn your prompt into video"
+        icon={Video}
+        iconColor="text-orange-500"
+        bgColor="bg-orange-500/10"
+      />
+
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid-cols-12 gap-2"
+            >
+              <FormField
+                control={form.control}
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-6">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent pl-4"
+                        placeholder="Clown fish swimming around"
+                        disabled={isLoading}
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              {/* <FormField
+                control={form.control}
+                name="num_outputs"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 md:col-span-2">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {amountOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              /> */}
+
+              {/* <FormField
+                control={form.control}
+                name="resolution"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 md:col-span-2">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {resolutionOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              /> */}
+
+              <Button
+                className="col-span-12 lg:col-span-2 bg-blue-600 w-fit mt-4 rounded-lg flex justify-end"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-20">
+              <Loader />
+            </div>
+          )}
+          {!videos && !isLoading && <Empty label="No video generated" />}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+            {videos && (
+              <video
+                className="w-full aspect-video mt-8 rounded-lg border bg-black"
+                controls
+              >
+                <source src={videos} />
+              </video>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoGeneration;
