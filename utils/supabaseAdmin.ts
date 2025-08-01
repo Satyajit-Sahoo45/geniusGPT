e3c250f44c0099f9e72343bc661f4df65146266c
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2025-08-01 13:03:41 +0530

    update supabase admin

--- utils/supabaseAdmin.ts
+++ utils/supabaseAdmin.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 import { Database } from "../types_db";
 
-const supabaseUrl = "https://iehgkylvtmkungcvzrni.supabase.co";
+const supabaseUrl = "https://zaklcntahdhosnjcfonm.supabase.co";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
